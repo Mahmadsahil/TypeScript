0befae2251123d5c03c7911e5fe772dc119d1d36
Md Sahil Achanoor <117533136+Mahmadsahil@users.noreply.github.com>
2024-11-13 19:43:44 +0530

    Add files via upload

---
+++ dist/variables.js
@@ -0,0 +1,8 @@
+"use strict";
+let UserName;
+let Phone;
+let email;
+UserName = "Md Sahil";
+Phone = 123456789;
+email = "sahli@gmail.com";
+console.log(`Hello ${UserName} Welcome to CIS, we'll reach out to you via Call : ${Phone} or email : ${email}`);
---
+++ src/variables.ts
@@ -0,0 +1,13 @@
+let UserName: string;
+
+let Phone: number;
+
+let email: string;
+
+UserName = "Md Sahil";
+Phone = 123456789;
+email = "sahli@gmail.com";
+
+console.log(
+  `Hello ${UserName} Welcome to CIS, we'll reach out to you via Call : ${Phone} or email : ${email}`
+);
